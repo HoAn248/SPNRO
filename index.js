db515a67f363f6f82f74b64238e240fe393ee653
HoAn <hda24082002@gmail.com>
2023-06-26 09:54:52 +0700

    update

---
+++ index.js
@@ -0,0 +1,29 @@
+let goc = document.getElementById('goc')
+let ao = document.getElementById('goc')
+let quan = document.getElementById('quan')
+let gang = document.getElementById('gang')
+let giay = document.getElementById('giay')
+let rada = document.getElementById('rada')
+let ct = document.getElementById('ct')
+let glt = document.getElementById('glt')
+let btn = document.getElementById('btn')
+
+btn.onclick = () => {
+    if (goc.value !== '') {
+
+        let allInput = document.querySelectorAll('.cclt')
+        let values = []
+        allInput.forEach(e => {
+            if (e.value !== '') {
+                values.push(parseFloat(e.value))
+            }
+        });
+        let currentGoc = parseFloat(goc.value)
+        values.forEach(e => {
+            currentGoc = currentGoc + (currentGoc * e) / 100
+        });
+        console.log(currentGoc);
+    } else {
+        alert('Nhập hộ cái chỉ số gốc cái!')
+    }
+}
